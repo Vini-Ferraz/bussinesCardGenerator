2ccbf125788df7f131479c12da68f3355792ec3c
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-03-13 16:59:28 -0300

    teste

--- bussines-card-generator/src/components/bussines-card-componentes/NomeComponente.jsx
+++ bussines-card-generator/src/components/bussines-card-componentes/NomeComponente.jsx
@@ -2,6 +2,6 @@ export function Nome({ nome, NomeTesteRegex }) {
   return nome && NomeTesteRegex ? (
     <h1 id="nome">{nome}</h1>
   ) : (
-    <h1 id="nome">Xxx X. Xxxxxx</h1>
+    <h1 id="nome">Xxx X. Xxxxx</h1>
   );
 }
