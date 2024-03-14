3df3eb71009fdb2b1b117ed5de804205d32a4dc8
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-03-14 19:21:19 -0300

    teste

--- bussines-card-generator/src/components/bussines-card-componentes/CargoComponente.jsx
+++ bussines-card-generator/src/components/bussines-card-componentes/CargoComponente.jsx
@@ -2,6 +2,6 @@ export function Cargo({ cargo, CargoTesteRegex }) {
   return cargo && CargoTesteRegex ? (
     <h2 id="cargo">{cargo}</h2>
   ) : (
-    <h2 id="cargo">Xxxxxxxx Xx Xx</h2>
+    <h2 id="cargo">Xxxxxxxx Xx Xxx</h2>
   );
 }
